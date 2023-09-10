70f4b348c817a62854c128ff0f28f171d9646c63
Jay Reddy <jayreddy@gmail.com>
2023-09-09 18:48:14 -0700

    adds struct method to hooper

--- second/2-3.go
+++ second/2-3.go
@@ -22,6 +22,12 @@ type country struct {
     population uint32
 }
 
+func (h1 hooper) isBetterOverall(h2 hooper) bool {
+    ok := h1.overall > h2.overall
+    fmt.Printf("is %s better than %s?:\n%v\n", h1.name, h2.name, ok)
+    return ok
+}
+
 func ThirdExercise() {
     melo := hooper{
             person: person{
@@ -39,6 +45,25 @@ func ThirdExercise() {
             overall: 81,
     }
 
+    jordan := hooper{
+        person: person{
+            name: "Michael Jordan",
+            heightInCm: 210,
+            weightInKg: 90,
+            favoriteFood: "winning",
+        },
+        nationality: country{
+            continent: "NA",
+            population: 123456789,
+        },
+        offense: 99,
+        defense: 99,
+        overall: 99,
+    }
+
     fmt.Printf("%+v\n", melo)
     fmt.Printf("%s\n", melo.nationality.continent)
+    
+    jordan.isBetterOverall(melo)
+    
 }
